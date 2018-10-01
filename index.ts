4afe2321c0341fb3e4c206cd0dabf6f276cd0b3e
Roman Bednyakov <rbednyakov@mentalstack.com>
2018-10-01 10:43:40 +0300

    array

--- index.ts
+++ index.ts
@@ -17,21 +17,21 @@
 // newunknow = 1;
 // newunknow = true;
 //
-// // базовые перемнный
-//
-// //Array
-// let numArray: number[] = [1, 1, 2];
-// let numArray2: Array<number> = [1, 1, 2];
-//
-// let strArray: string[] = ['sda', 'ad', 'sad'];
-// let strArray2: Array<string> = ['sda', 'ad', 'sad'];
-//
-// let booleArray: boolean[] = [true, false];
-// let booleArray2: Array<boolean> = [true, false];
-// // tuples
-// let array: [number, number, string] = [1, 2, '3'];
-// let array2: [boolean, number, string] = [true, 2, '3'];
-//
+// базовые перемнный
+
+//Array
+let numArray: number[] = [1, 1, 2];
+let numArray2: Array<number> = [1, 1, 2];
+
+let strArray: string[] = ['sda', 'ad', 'sad'];
+let strArray2: Array<string> = ['sda', 'ad', 'sad'];
+
+let booleArray: boolean[] = [true, false];
+let booleArray2: Array<boolean> = [true, false];
+// tuples
+let array: [number, number, string] = [1, 2, '3'];
+let array2: [boolean, number, string] = [true, 2, '3'];
+
 //Функции
 
 function getMyAge(age: number, prefix: string = 'wfm-'): string {

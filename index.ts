a9fe817275c54c2b55e7c3413735c2a60a7ede65
Roman Bednyakov <rbednyakov@mentalstack.com>
2018-10-08 13:04:24 +0300

    str

--- index.ts
+++ index.ts
@@ -1,38 +1,38 @@
-// базовые перемнный
-//string
-let str: string = 'strsssing!';
-
-//number
-let num: number = 10;
-
-//booleam
-let isTrue: boolean = true;
-let isFalse: boolean = false;
-
-//other
-let unknow: string | number = '1';
-unknow = 1;
-unknow = 'sd';
-let newunknow: any = '1';
-newunknow = 1;
-newunknow = true;
-
-//базовые перемнный
-
-//Array
-let numArray: number[] = [1, 1, 2];
-let numArray2: Array<number> = [1, 1, 2];
-
-let strArray: string[] = ['sda', 'ad', 'sad'];
-let strArray2: Array<string> = ['sda', 'ad', 'sad'];
-
-let booleArray: boolean[] = [true, false];
-let booleArray2: Array<boolean> = [true, false];
-// tuples
-let array: [number, number, string] = [1, 2, '3'];
-let array2: [boolean, number, string] = [true, 2, '3'];
-
-// //Функции
+// // базовые перемнный
+// //string
+// let str: string = 'strsssing!';
+//
+// //number
+// let num: number = 10;
+//
+// //booleam
+// let isTrue: boolean = true;
+// let isFalse: boolean = false;
+//
+// //other
+// let unknow: string | number = '1';
+// unknow = 1;
+// unknow = 'sd';
+// let newunknow: any = '1';
+// newunknow = 1;
+// newunknow = true;
+//
+// //базовые перемнный
+//
+// //Array
+// let numArray: number[] = [1, 1, 2];
+// let numArray2: Array<number> = [1, 1, 2];
+//
+// let strArray: string[] = ['sda', 'ad', 'sad'];
+// let strArray2: Array<string> = ['sda', 'ad', 'sad'];
+//
+// let booleArray: boolean[] = [true, false];
+// let booleArray2: Array<boolean> = [true, false];
+// // tuples
+// let array: [number, number, string] = [1, 2, '3'];
+// let array2: [boolean, number, string] = [true, 2, '3'];
+//
+// // //Функции
 //
 // function getMyAge(age: number, prefix: string = 'wfm-'): string {
 //     return prefix + age
